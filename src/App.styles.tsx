11d890b54e454342eaf86c23103c8db19e260159
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2022-05-15 17:51:40 +0200

    Create home page

---
+++ src/App.styles.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const App = styled.div`
+  text-align: center;
+`;
+
+export const Header = styled.header`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const HeaderTitleLink = styled.a`
+  text-decoration: none;
+  margin: 20vmin 0 0 0;
+`;
+export const HeaderTitle = styled.h1`
+  color: #6099ff;
+  font-size: 1.4rem;
+`;
+export const HeaderTitleLogo = styled.img`
+  margin: 0 -0.4rem 0 0;
+`;
+
+export const HeaderMenu = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 2rem;
+  margin: 14vmin 0 20vmin 0;
+`;
+
+export const HeaderMenuItem = styled.a`
+  width: 200px;
+  color: #2a2a2a;
+  background: #fff;
+  border: 1px solid #00000018;
+  border-radius: 1rem;
+  text-align: left;
+  text-decoration: none;
+  padding: 1rem 2rem;
+  transition: box-shadow 0.3s ease-in-out;
+  font-size: 1rem;
+  &:hover {
+    box-shadow: 0 1rem 2rem #00000008, 0 0.6rem 1rem #00000010;
+  }
+  h2 {
+    font-size: 1.2rem;
+  }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as S from "./App.styles";
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <S.App>
+      <S.Header>
+        <S.HeaderTitleLink href="/">
+          <S.HeaderTitle>
+            <S.HeaderTitleLogo src={logo} alt="logo" />
+            education
+          </S.HeaderTitle>
+        </S.HeaderTitleLink>
+        <S.HeaderMenu>
+          <S.HeaderMenuItem href="/developer">
+            <>
+              <h2>Developer &rarr;</h2>
+              <p>Development guide</p>
+            </>
+          </S.HeaderMenuItem>
+          <S.HeaderMenuItem href="/designer">
+            <>
+              <h2>Designer &rarr;</h2>
+              <p>Design guide</p>
+            </>
+          </S.HeaderMenuItem>
+          <S.HeaderMenuItem href="/salesperson">
+            <>
+              <h2>Salesperson &rarr;</h2>
+              <p>Store management guide</p>
+            </>
+          </S.HeaderMenuItem>
+        </S.HeaderMenu>
+      </S.Header>
+    </S.App>
   );
 }
 
